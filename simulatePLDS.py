833441b80d550becd5798cd46db47ec183dbff78
amoretti86 <amoretti86@gmail.com>
2016-11-27 21:48:57 -0500

    Update and rename mvstatespace.py to simulatePLDS.py

--- simulatePLDS.py
+++ simulatePLDS.py
@@ -1,16 +1,10 @@
 '''
-simulte data to test the auxiliary particle filter according to the following recursions:
+Creates synthetic data for the Poisson Linear Dynamical System according to the following recursions:
 
-x_1 = x_0 + \sigma \epsilon_0
-x_t = a*x_{t-1} + \sigma*\epsilon_t
-y_t = b*x_t + \gamma*\eta_t
+x_t = Ax_{t-1} + Sigma^{1/2}\epsilon    where x ~ N(Ax_{t,1}, Sigma^{1/2})
+y_t = Pois(exp(Bx_t)) + \eta            where y ~ Pois(exp(Bx_t))
 
-\epsilon_t ~ N(0, 1)
-\eta_t ~ N(0, 1)
-
-parameters:
-a, b, \sigma, \gamma, T, x_0
-
+by Antonio Moretti - amoretti@cs.columbia.edu
 '''
 
 import numpy as np
@@ -20,12 +14,7 @@ import matplotlib.pyplot as plt
 def simulateStateSpace(A,B,Sigma,T,x_0, plot = True):
     '''
     Generates synthetic data for a state space model according to the recursions above.
-    a and sigma are the terms acting on x and epsilon
-    b and gamma are the terms acting on y and eta
-    epsilon and gamma are standard white noise
-    y is a hidden variable linearly dependent upon x the noisy observations
     '''
-
     if Sigma.any < 0:
         print "Sigma must be PSD"
         return
@@ -33,27 +22,23 @@ def simulateStateSpace(A,B,Sigma,T,x_0, plot = True):
     X = np.zeros((T,2))
     Y = np.zeros((T,2))
     time = np.arange(T)
-    X[0] = x_0#np.random.normal(0,1,1)#x_0
+    X[0] = x_0
     CS = np.linalg.cholesky(Sigma)
 
     for i in range(1,T):
-        #X[i] = a*X[i-1] + sigma*np.random.normal(0,1,1)
-        #X[i,:] = np.dot(A, X[i-1,:])*np.random.randn(1,2)[0]
-        #Y[i] = np.random.poisson(np.exp(b*X[i]))
         X[i,:] = np.dot(A, X[i-1,:])+np.dot(CS,np.random.randn(1,2)[0])
         Y[i,:] = np.random.poisson(np.exp(np.dot(B,X[i,:])))
-        #Y[i,1] = np.random.poisson(np.exp(B[1]*X[i,1]))
 
     if plot == True:
-        #plt.subplot(121)
         
+        plt.subplot(121)
         lo = plt.plot(time, X[:,0], 'r', time, Y[:,0], 'b')
         plt.xlabel('time')
         plt.ylabel('signal')
         plt.title('signal in first dim')
         plt.legend(lo, ('X', 'Y'))
         plt.show()
-        #plt.subplot(122)
+        plt.subplot(122)
         lo = plt.plot(time, X[:,1], 'r', time, Y[:,1], 'b')
         plt.xlabel('time')
         plt.ylabel('signal')
@@ -65,20 +50,9 @@ def simulateStateSpace(A,B,Sigma,T,x_0, plot = True):
     return X, Y
 
 if __name__ == "__main__":
-    '''
-    a = .9
-    b = 0.25
-    sigma = .5
-    #gamma = .24
-    T = 1000
-    x_0 = np.random.normal(0,1,1)
-    print x_0[0]
-    [hidden, obs] = simulateStateSpace(a,b,sigma,T, x_0)#x_0)
-    '''
+
     A = np.asarray([.65, .24])
     Sigma = np.asarray([[1, .5],[.5, 1]])
-    #print A
-    #print sigma
     B = np.asarray([.24, .26])
     T = 100
     x_0 = np.random.randn(1, 2)[0]
